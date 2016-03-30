1f8797a8895182a3806914a2cff357a5a6a715e8
andreiVaduva <vaduva.andrei.iustin@gmail.com>
2016-03-30 13:00:43 +0300

    Practical test 01 first attempt

---
+++ PracticalTest01/app/src/main/java/practicaltest01/eim/systems/cs/pub/ro/practicaltest01/PracticalTest01Service.java
@@ -0,0 +1,34 @@
+package practicaltest01.eim.systems.cs.pub.ro.practicaltest01;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+/**
+ * Created by andrei on 30.03.2016.
+ */
+public class PracticalTest01Service extends Service {
+    private ProcessingThread processingThread = null;
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        int firstNumber = intent.getIntExtra("firstNubmer", -1);
+        int secondNumber = intent.getIntExtra("secondNumber", -1);
+        processingThread = new ProcessingThread(this, firstNumber, secondNumber);
+        processingThread.start();
+        return Service.START_REDELIVER_INTENT;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        processingThread.stopThread();
+    }
+}
